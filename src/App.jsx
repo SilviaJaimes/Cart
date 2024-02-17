dcbecec4a6beb97450fa5b19b137a983a0e6c006
SilviaJaimes <jaimessilvia338@gmail.com>
2024-02-16 22:14:47 -0500

    feat: :bug:

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import { products as initialProducts } from './mocks/products.json';
 import Header from './components/Header.jsx'
 import Products from './components/Products.jsx';
 import Footers from './components/Footers.jsx';
-import { IS_DEVELOPMENT } from './config.js';
+/* import { IS_DEVELOPMENT } from './config.js'; */
 import useFilters from './hooks/useFilters.js';
 import { CartProvider } from './context/cart.jsx';
 import NavbarPrincipal from './components/NavbarPrincipal.jsx';
 
 export default function App() {
   const { filterProducts } = useFilters();
-  const filteredProudcts = filterProducts(initialProducts);
+  const filteredProducts = filterProducts(initialProducts);
 
   return (
     <CartProvider>
       <NavbarPrincipal/>
       <Header/>
-      <Products products={filteredProudcts}/>
-      { IS_DEVELOPMENT && <Footers /> } 
+      <Products products={filteredProducts}/>
+      {/* { IS_DEVELOPMENT && <Footers /> }  */}
+      <Footers />
     </CartProvider>
   );
 };
